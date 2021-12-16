c9ac56f57ec21676cbd09bf01735b7f235aa066d
Florian Rolland <florian.rolland@outlook.com>
2021-12-16 10:14:54 +0100

    Prototype fichier lecteurCarte.c

--- lecteurcarte.c
+++ lecteurcarte.c
@@ -6,20 +6,45 @@ void lecteurcarte_initialiser()
 }
 
 void lecteurcarte_lire_carte(){
+  unsigned short int numero_carte;
+  //Si la borne est disponible
+  if(get_voyant_Disponible == VERT){
+    if(carte_inseree()){
+      //Lancement du timer
+      //Verification avec numero_carte
+      if(1){
+        //On fait clignoter le bouton 8s
+        clignotement_voyant_Charge();
+        //Tant qu'il y a le timer, on attend appuie bouton
+        while(true){
+          if(bouton_charge() == 1){
+            set_voyant_Disponible(ROUGE);
+            genCharge();
+            break;
+          }
+          else{//Erreur authentification
+            clignotement_voyant_Defaut();
+            break;
+          }
+        }
+      }
+      else{//Erreur authentification
+        clignotement_voyant_Defaut();
+      }
+    }
+  }
+  //Si borne non disponible
+  else{
+    if(carte_inseree){ //Si une carte inséré
+      if(1){ //Si client authentifier
+        if(get_voyant_Charge() == VERT){ //Si la charge n'est pas active -> Charge terminé ou STOP
+        //UC Reprendre vehicule
+        }
+      }
+    }
+    else{//Erreur authentification
+    clignotement_voyant_Defaut();
+  }
 
-  //Verification dispo borne
-  //Insertion carte
-
-  //Authentifacation client
-
-  //Cas echec authentification
-
-
-  //Cas réussi
-
-  //Si appuie sur bouton Charge -> UC CHARGEMENT
-
-
-
-  //Si pas appuie bouton Charge -> ARRETE
+  }
 }
