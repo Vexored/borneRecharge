a689971f4221154dba7f4d86e2c9c6fde8ae6764
Florian Rolland <florian.rolland@outlook.com>
2021-12-16 10:14:33 +0100

    Ajout Voyant "Defaut"

--- voyant.c
+++ voyant.c
@@ -13,3 +13,11 @@ set_voyant_Disponible(led etat){
   io = acces_memoire(&shmid);
   io->led_dispo = etat;
 }
+get_voyant_Charge(){
+  io = acces_memoire(&shmid);
+  return io->led_charge;
+}
+get_voyant_Disponible(){
+  io = acces_memoire(&shmid);
+  return io->led_dispo;
+}
